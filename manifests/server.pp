a34197de5a6afad8abebf730300d04fea58bca4a
Anand Kumria <akumria@acm.org>
2012-05-20 12:45:53 +0100

    Create the server configuration file

--- manifests/server.pp
+++ manifests/server.pp
@@ -7,8 +7,18 @@ class postgresql::server (
 ) inherits postgresql::params {
 
   package { "postgresql-server-$version":
-    name   => "$server_package-$version",
-    ensure => present,
+    name    => "$server_package-$version",
+    ensure  => present,
+  }
+
+  file { "postgresql-server-$version-config":
+    name    => "/etc/postgresql/$version/main/postgresql.conf",
+    ensure  => present,
+    content => template(postgresql.conf),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package[postgresql-server-$version],
   }
 
 }
