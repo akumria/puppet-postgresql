4042e053126da4c719519e1d4aa0f3d51b841785
Anand Kumria <akumria@acm.org>
2012-06-11 15:56:03 +0200

    Add a type so we can also manage postgres databases

---
+++ lib/puppet/type/pg_database.rb
@@ -0,0 +1,17 @@
+# This has to be a separate type to enable collecting
+Puppet::Type.newtype(:pg_database) do
+  @doc = "Manage Postgresql databases."
+
+  ensurable
+
+  newparam(:name, :namevar=>true) do
+    desc "The name of the database."
+  end
+
+  newparam(:charset) do
+    desc "The character set to use for the database"
+    defaultto :utf8
+    newvalue(/^\S+$/)
+  end
+
+end
