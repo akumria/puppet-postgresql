92ef4d0c2c094c3f96a95376e6d32f413cf691aa
Anand Kumria <akumria@acm.org>
2012-06-16 14:53:08 +0200

    Add a shortcut to add both a user and database with the same name.
    This makes the common case of 'name' being used for the username and
    database name simpler to configure correctly.

---
+++ manifests/db.pp
@@ -0,0 +1,20 @@
+define postgresql::db (
+    $owner,
+    $password,
+    $encoding = 'UTF8',
+    $locale = 'en_GB.UTF-8',
+) {
+
+    pg_user {$owner:
+        ensure      => present,
+        password    => $password,
+    }
+
+    pg_database {$name:
+        ensure      => present,
+        owner       => $owner,
+        require     => Pg_user[$owner],
+        encoding    => $encoding,
+        locale      => $locale,
+    }
+}
