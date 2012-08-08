f1d13ddada29f3bc8bd91deea780e6df211134cc
Anand Kumria <akumria@acm.org>
2012-08-08 09:20:03 +0100

    We now manage the pg_hba.conf file as well

--- manifests/server.pp
+++ manifests/server.pp
@@ -33,4 +33,15 @@ class postgresql::server (
     notify  => Service["postgresql-system-$version"],
   }
 
+  file { "postgresql-server-hba-config-$version":
+    name    => "/etc/postgresql/$version/main/pg_hba.conf",
+    ensure  => present,
+    content => template('postgresql/pg_hba.conf.erb'),
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '0640',
+    require => Package["postgresql-server-$version"],
+    notify  => Service["postgresql-system-$version"],
+  }
+
 }
