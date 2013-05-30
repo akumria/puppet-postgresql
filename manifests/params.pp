9585b4838635d1a4f0519c4e7a3d7ac8e2f2c961
Tom De Vylder <tom@penumbra.be>
2013-05-30 17:03:37 +0200

    Debian Squeeze fixes

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,11 +10,15 @@ class postgresql::params {
 
   case $::operatingsystem {
     /(Ubuntu|Debian)/: {
-      $version = '9.1'
       $client_package = 'postgresql-client'
       $server_package = 'postgresql'
       $listen_address = 'localhost'
       $port = 5432
+
+      $version = $::lsbdistcodename ? {
+        'squeeze' => '8.4',
+        default   => '9.1',
+      }
 
       $service_name = $::lsbdistcodename ? {
         'wheezy' => 'postgresql',
